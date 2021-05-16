4da6bae78810bbd0764295781fff9de61e295466
Umaru <lchueca@ucm.es>
2021-05-16 12:56:29 +0200

    'InicioiMockito'

--- src/test/main/application/service/UserServiceImplTest.java
+++ src/test/main/application/service/UserServiceImplTest.java
@@ -1,71 +1,126 @@
 package main.application.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.regex.Pattern;
+import java.util.Properties;
+
 import lombok.var;
+import main.domain.converter.PublicacionConverter;
+import main.domain.converter.UsuarioConverter;
+import main.domain.converter.PreviewPublicacionConverter;
+
+import main.domain.resource.UsuarioResource;
 import main.domain.resource.PreviewPublicacion;
 import main.domain.resource.PublicacionResource;
-import main.domain.resource.UsuarioResource;
 import main.domain.resource.ValoracionResource;
+
+import main.persistence.entity.Usuario;
+import main.persistence.repository.RepoUsuario;
+import main.persistence.entity.Publicacion;
+
+
+
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceImplTest {
 
     @Autowired
     UserServiceImpl userService;
 
+    @Mock
+    RepoUsuario repoUsuarioMock;
+    UsuarioConverter converterUserMock;
+
 
     @Test
-    void getUserByName() {
+    void getUserByName() { //TEST X
 
-        //Comprueba que devuelve el mismo nombre
-        userService = new UserServiceImpl();
-        String nombre = "Grogu";
-        UsuarioResource usuario =  userService.getUserByName(nombre);
-        Assertions.assertEquals("Grogu", usuario.getName());
+        assertNotNull(repoUsuarioMock);
+
+        //DEVUELVE UN TIPO USUARIO
+        //Creamos un tipo usuario y solo rellenamos lo que nos interesa
+        UsuarioResource usuario = new UsuarioResource();
+        usuario.setName("Grogu");
 
 
-        //Le envio uno que no existe y tiene que devolver null;
-        userService = new UserServiceImpl();
-        String nombrenull = "Aslan";
-        UsuarioResource usuarioNull = userService.getUserByName(nombrenull);
-        Assertions.assertNull(usuarioNull);
+        when(converterUserMock.convert(repoUsuarioMock.findByName("Grogu"))).thenReturn(usuario);
+        assertEquals("Grogu", usuario.getName());
+
+
+        when(converterUserMock.convert(repoUsuarioMock.findByName("Gro"))).thenReturn(usuario);
+        assertEquals("Grogu", usuario.getName());
+
+
+        //DEVUELVE UN NULL
+        usuario = null;
+        when(converterUserMock.convert(repoUsuarioMock.findByName("Aslan"))).thenReturn(usuario);
+        assertNull(usuario);
 
     }
 
+
+    @Mock
+    List<Publicacion> ListPubliMock;
+    PreviewPublicacionConverter converterPreviewMock;
+    List<PreviewPublicacion> ListPrePubliMock;
+
     @Test
-    void getPosts() {
+    void getPosts() { //Test X
 
-        //Con un usuario existente, comprobamos que se devuelve una lista de post
-        userService = new UserServiceImpl();
-        String nombre = "Grogu";
-        List<PreviewPublicacion> postUsuario =  userService.getPosts(nombre);
-        Assertions.assertNotNull(postUsuario);
+        assertNotNull(ListPubliMock);
 
-        //Le envio un usuario que no existe y tiene que devolver null;
-        userService = new UserServiceImpl();
-        String nombreNull = "Grogu";
-        List<PreviewPublicacion> postUsuarioNull =  userService.getPosts(nombreNull);
-        Assertions.assertNull(postUsuarioNull);
+        //DEVUELVE UNA LISTA
+        when(ListPubliMock.stream().map(converterPreviewMock::convert).collect(Collectors.toList())).thenReturn(ListPrePubliMock);
+        assertNotNull(ListPrePubliMock);
+
+
+        //DEVUELVE NULL
+        ListPrePubliMock = null;
+        when(ListPubliMock.stream().map(converterPreviewMock::convert).collect(Collectors.toList())).thenReturn(ListPrePubliMock);
+        assertNull(ListPrePubliMock);
+
     }
 
-    @Test
-    void upload() {
-        //Con un usuario existente, comprobamos que el usuario existe y se manda una fotografía
-        userService = new UserServiceImpl();
-        String nombre = "Grogu";
-        String texto = "Que rico!";
-        String localizacion = "Dagobah";
+    @Mock
+    //repoUsuarioMock (Arriba)
+    PublicacionResource publiResoMock;
+    Pattern imagePAtMock;
+    MultipartFile imageMock;
+    PublicacionConverter converterPubliMock;
+    Publicacion publiMock;
 
-        // MultipartFile foto = (nombre,  );
-        // PublicacionResource postUsuario =  userService.upload();
-        //Assertions.assertNotNull(postUsuario);
+    @Test
+    void upload() { //Test X
+        assertNotNull(repoUsuarioMock);
+        Usuario usuario = null;
+
+        //DEVUELVE NULL
+        when(repoUsuarioMock.findByName("Aslan")).thenReturn(usuario);
+        assertNull(usuario);
+
+        //EXCEPCION NO SE PUEDE GUARDAR LA IMAGEN
+        Properties properties = Mockito.mock(Properties.class);
+        when(imagePAtMock.matcher(imageMock.getOriginalFilename())).thenThrow(new IllegalArgumentException("Only jpeg and png images are supported."));
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> properties.get("Only jpeg and png images are supported."));
+        assertEquals("Only jpeg and png images are supported.",exception.getMessage());
+
+        //DEVUELVE UNA PUBLICACION
+        when(converterPubliMock.convert(publiMock)).thenReturn(publiResoMock);
+        assertNotNull(publiResoMock);
+
     }
 
     @Test
