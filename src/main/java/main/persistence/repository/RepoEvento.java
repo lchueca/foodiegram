0659036e6b2cf29775c72e4f9c421276e4a35724
JC-bip <jorgecanopaez@gmail.com>
2021-05-18 12:57:29 +0200

    toda la parte sobre eventos

--- src/main/java/main/persistence/repository/RepoEvento.java
+++ src/main/java/main/persistence/repository/RepoEvento.java
@@ -1,9 +1,15 @@
 package main.persistence.repository;
 
 import main.persistence.entity.Evento;
+import main.persistence.entity.Publicacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RepoEvento extends JpaRepository<Evento,Integer> {
+public interface RepoEvento extends JpaRepository<Evento, Integer> {
+
+    Evento findById(Integer id);
+    List<Evento> findByIdcolab(Integer idcolab);
 }
