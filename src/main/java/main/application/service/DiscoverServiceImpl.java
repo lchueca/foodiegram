d7eda85d0c027fc9d8579b156cb7398ddcf6c1e5
gulis1 <juli240820@gmail.com>
2021-05-27 02:57:33 +0200

    En principio ya esta develop

--- src/main/java/main/application/service/DiscoverServiceImpl.java
+++ src/main/java/main/application/service/DiscoverServiceImpl.java
@@ -1,14 +1,16 @@
 package main.application.service;
 
 
+import main.domain.converter.PreviewColabJOINUserConverter;
 import main.domain.converter.PreviewPublicacionConverter;
-import main.domain.converter.PublicacionConverter;
-import main.domain.converter.UsuarioConverter;
+import main.domain.converter.PreviewUserConverter;
+import main.domain.resource.PreviewColabJOINUser;
 import main.domain.resource.PreviewPublicacion;
-import main.domain.resource.PublicacionResource;
-import main.domain.resource.UsuarioResource;
+import main.domain.resource.PreviewUsuario;
+import main.persistence.entity.ColabJOINUser;
 import main.persistence.entity.Publicacion;
 import main.persistence.entity.Usuario;
+import main.persistence.repository.RepoColabJOINUser;
 import main.persistence.repository.RepoPublicacion;
 import main.persistence.repository.RepoUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,75 +29,90 @@ public class DiscoverServiceImpl implements DiscoverService {
     @Autowired
     private RepoUsuario repoUsuario;
 
-    private PublicacionConverter publicacionConverter= new PublicacionConverter();
-    private UsuarioConverter usuarioConverter = new UsuarioConverter();
-    private PreviewPublicacionConverter previewConverter = new PreviewPublicacionConverter();
+    @Autowired
+    private RepoColabJOINUser repoColabJOINUser;
+
+    private PreviewColabJOINUserConverter previewColabJOINUserConverter= new PreviewColabJOINUserConverter();
+    private PreviewUserConverter usuarioConverter = new PreviewUserConverter();
+    private PreviewPublicacionConverter publicacionConverter = new PreviewPublicacionConverter();
 
 
     public List<PreviewPublicacion> discoverByAmigo(){
 
         Integer userid=Integer.parseInt(SecurityContextHolder.getContext().getAuthentication().getName());
 
-        List<Publicacion> publi=repoPublicacion.findbyFriend(userid);
+        List<Publicacion> publi = repoPublicacion.fromFriends(userid);
 
         if(publi==null)
             return  null;
 
-        return publi.stream().map(previewConverter::convert).collect(Collectors.toList());
+        return publi.stream().map(publicacionConverter::convert).collect(Collectors.toList());
 
     }
 
 
-    public List<PreviewPublicacion> discoverBestRated(String period) throws IllegalArgumentException{
+    public List<PreviewPublicacion> discoverBestRated(String period, String country, String city) throws IllegalArgumentException{
 
-        Integer quantity = 1;
-
-        if (!period.equals("month") && !period.equals("day") && !period.equals("week") && !period.equals("year") && !period.equals("allTime"))
-            throw new IllegalArgumentException("Invalid period string.");
-
-        if (period.equals("allTime")) {
-            quantity = 99;
-            period = "year";
-        }
-
-        List<Publicacion> publi = repoPublicacion.discoverBestRated(quantity, period);
+        List<Publicacion> publi = repoPublicacion.bestRated(getDayAmount(period), country, city);
 
         if(publi==null)
             return  null;
 
-        return publi.stream().map(previewConverter::convert).collect(Collectors.toList());
+        return publi.stream().map(publicacionConverter::convert).collect(Collectors.toList());
     }
 
     @Override
     public List<PreviewPublicacion> discoverMostRated(String period) {
-        Integer quantity = 1;
 
-        if (!period.equals("month") && !period.equals("day") && !period.equals("week") && !period.equals("year") && !period.equals("allTime"))
-            throw new IllegalArgumentException();
-
-        if (period.equals("allTime")) {
-            quantity = 99;
-            period = "year";
-        }
-
-        List<Publicacion> publi = repoPublicacion.discoverMostRated(quantity, period);
+        List<Publicacion> publi = repoPublicacion.mostRated(getDayAmount(period));
 
         if(publi==null)
             return  null;
 
-        return publi.stream().map(previewConverter::convert).collect(Collectors.toList());
+        return publi.stream().map(publicacionConverter::convert).collect(Collectors.toList());
     }
 
     @Override
-    public List<UsuarioResource> findFollowedByFriends(Integer userid) {
+    public List<PreviewUsuario> findFollowedByFriends(Integer userid) {
         List<Usuario> list = repoUsuario.findFollowedByFriends(userid);
 
         return list.stream().map(usuarioConverter::convert).collect(Collectors.toList());
     }
 
     @Override
-    public List<PreviewPublicacion> popularSameCity(Double lat, Double lon) {
-        return null;
+    public List<PreviewUsuario> userWhoFollowXAlsoFollowY(String userName) {
+
+        Usuario user = repoUsuario.findByName(userName);
+
+        if (user == null)
+            return null;
+
+        List<Usuario> list = repoUsuario.usersFollowedByUsersWhoFollow(user.getId());
+
+        return list.stream().map(usuarioConverter::convert).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<PreviewColabJOINUser> findCollabs(String country, String city) {
+
+        List<ColabJOINUser> collab=repoColabJOINUser.descubrirCollab(country,city);
+
+        return collab.stream().map(previewColabJOINUserConverter::convert).collect(Collectors.toList());
+    }
+
+
+    private Integer getDayAmount(String periodo) {
+
+        switch (periodo) {
+            case "day": return 1;
+            case "week": return 7;
+            case "month": return 30;
+            case "year": return 365;
+            case "allTime": return -1;
+            default: throw new IllegalArgumentException("Invalid period");
+        }
+
+
     }
 
 
